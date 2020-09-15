78a6c74b1cd0ae93cd29b074e7a9b9dedced4223
Rytis <rytis.augustauskas1@gmail.com>
2020-09-15 12:28:13 +0300

    Working listener

--- modules/trainModel.py
+++ modules/trainModel.py
@@ -1,4 +1,10 @@
 import tensorflow
 
-def test():
+def test(a, b):
     print("Hello World From Python!55555")
+    sum = a + b
+    print(str(sum))
+    tensorflow.print("print from tensorflow")
+
+def test2():
+    print("Hello World From Python second time!")
